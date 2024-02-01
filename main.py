897ec1e5dfb704b4f66c64d1608438c4d8b78ed3
kasloww <kdavidson@brooklynsteamcenter.org>
2024-02-01 10:54:41 -0500

    make sure to finish sign up

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('tive.jinja')
+    return render_template('tive.html.jinja')
 
+@app.route('/register')
+def reg():
+    return render_template('tive.signup.jinja')
+if __name__ == '__main__':
+    app.run()
 @app.route('/Tive/<name>')
 def tive(name):
     return 'Welcome to ' + name
