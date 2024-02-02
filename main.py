62a7bf3b839ed4415ba7376559072f21bec120d3
kasloww <kdavidson@brooklynsteamcenter.org>
2024-02-02 10:53:14 -0500

    sign up is done, just do designs if want to

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import pymysql
+import pymysql.cursors
 app = Flask(__name__)
+
+conn = pymysql.connect(
+    database="kdavidson_",
+    user="kdavidson",
+    password="231534561",
+    host="10.100.33.60",
+    cursorclass=pymysql.cursors.DictCursor
+)
 
 @app.route('/')
 def index():
     return render_template('tive.html.jinja')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def reg():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        cursor = conn.cursor()
+        cursor.execute(f"INSERT INTO `Users` (`User`, `Password`) VALUES ('{username}', '{password}')")
+        cursor.close()
+        conn.commit()
     return render_template('tive.signup.jinja')
 if __name__ == '__main__':
     app.run()
-@app.route('/Tive/<name>')
-def tive(name):
-    return 'Welcome to ' + name
