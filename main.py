9be37a28283ba3f629b52f0b764d43146d4950ec
kasloww <kdavidson@brooklynsteamcenter.org>
2024-02-06 10:55:28 -0500

    login works :D

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import pymysql
 import pymysql.cursors
 app = Flask(__name__)
@@ -10,7 +10,8 @@ conn = pymysql.connect(
     host="10.100.33.60",
     cursorclass=pymysql.cursors.DictCursor
 )
-
+if __name__ == '__main__':
+    app.run()
 @app.route('/')
 def index():
     return render_template('tive.html.jinja')
@@ -25,5 +26,21 @@ def reg():
         cursor.close()
         conn.commit()
     return render_template('tive.signup.jinja')
-if __name__ == '__main__':
-    app.run()
+    
+@app.route('/signin', methods=['GET', 'POST'])
+def sign():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT * FROM `Users` WHERE `User` = '{username}'")
+        user = cursor.fetchone()
+        cursor.close()
+        conn.commit()
+        if password == user["Password"]:
+            return redirect('/feed')
+    return render_template('tive.signin.jinja')
+
+@app.route('/feed')
+def fed():
+    return render_template('tive.feed.jinja')
